9b6108ff28a4989a348eaeb2838491c9e2f0eccb
齐佳 <qijia@yundun.com>
2018-04-08 14:31:29 +0800

    update build

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -18,11 +18,14 @@ fs.open('./build/env.js', 'w', function(err, fd) {
 
 module.exports = merge(webpackBaseConfig, {
   output: {
-    publicPath: 'https://iview.github.io/iview-admin/dist/', // 修改 https://iv...admin 这部分为你的服务器域名
+    publicPath: '/dist/', // 修改 https://iv...admin 这部分为你的服务器域名
     filename: '[name].[hash].js',
     chunkFilename: '[name].[hash].chunk.js'
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': 'production'
+    }),
     new cleanWebpackPlugin(['dist/*'], {
       root: path.resolve(__dirname, '../')
     }),
--- config/url.conf.js
+++ config/url.conf.js
@@ -1,4 +1,12 @@
-const AssetsIP = '114.55.219.41:8092' // dev service
-// const AssetsIP = '192.168.2.190:8092' // local service
+const isProduction = process.env.NODE_ENV === 'production'
+
+let AssetsIP
+
+if (isProduction) {
+  AssetsIP = '/'
+} else {
+  AssetsIP = '114.55.219.41:8092' // dev service
+  // AssetsIP = '192.168.2.190:8092' // local service
+}
 
 export { AssetsIP }
