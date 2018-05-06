98c21b80c5948cbda1a4471ba9fc51c299d5bfc4
AlenQi <15601739907@163.com>
2018-05-06 21:29:49 +0800

    update

--- src/views/vocation/down-pdf/downPdf.vue
+++ src/views/vocation/down-pdf/downPdf.vue
@@ -228,22 +228,18 @@ export default {
     },
     tableExport(type) {
       html2canvas(document.getElementById('renderPdf'), {
+        height: 10000,
         onrendered: function(canvas) {
+          document.getElementById('renderPdf').appendChild(canvas)
           var contentWidth = canvas.width
           var contentHeight = canvas.height
-          //一页pdf显示html页面生成的canvas高度;
           var pageHeight = contentWidth / 592.28 * 841.89
-          //未生成pdf的html页面高度
           var leftHeight = contentHeight
-          //页面偏移
           var position = 0
-          //a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
           var imgWidth = 555.28
           var imgHeight = 552.28 / contentWidth * contentHeight
           var pageData = canvas.toDataURL('image/jpeg', 1.0)
           var pdf = new jsPDF('', 'pt', 'a4')
-          //有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
-          //当内容未超过pdf一页显示的范围，无需分页
           if (leftHeight < pageHeight) {
             pdf.addImage(pageData, 'JPEG', 20, 0, imgWidth, imgHeight)
           } else {
@@ -251,7 +247,6 @@ export default {
               pdf.addImage(pageData, 'JPEG', 20, position, imgWidth, imgHeight)
               leftHeight -= pageHeight
               position -= 841.89
-              //避免添加空白页
               if (leftHeight > 0) {
                 pdf.addPage()
               }
@@ -359,4 +354,7 @@ export default {
 <style lang="less">
 @import 'networkSafe.less';
 @import 'keyBasics.less';
+* {
+  overflow: visible;
+}
 </style>
